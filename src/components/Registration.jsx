0737a12bfee36b0b5815ab9db49b70fe1e8a4b02
Lucie Trojanová <trojanovalucie1@gmail.com>
2023-10-11 22:45:00 +0200

    provázání obsahu s user, propojení emailu s User Name, výpis do k

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,41 +1,86 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { useState } from 'react';
-
-const user = {
-  username: '',
-  email: '',
-  password: '',
-  passwordConfirm: '',
-};
-
-const usernameField = () => {
-  const [username, setUsername] = useState('');
-};
+import { useState, useEffect } from 'react';
 
 export const Registration = (props) => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const updateUsername = (name) => {
+    setUser({ ...user, username: name });
+  };
+
+  const updatePassword = (pwd) => {
+    setUser({ ...user, password: pwd });
+  };
+
+  const updatePasswordConfirm = (pwd) => {
+    setUser({ ...user, passwordConfirm: pwd });
+  };
+
+  const updateEmail = (newemail) => {
+    setUser({ ...user, email: newemail });
+  };
+
+  const updateEmailBlur = (newemail) => {
+    if (user.username === '' && newemail.includes('@')) {
+      const email_split = newemail.split('@');
+      setUser({ ...user, username: email_split[0] });
+    }
+  };
+
+  const btnClick = (e) => {
+    console.log(user);
+    e.preventDefault();
+  };
+
   return (
     <form>
       <label>
-        username{' '}
+        username
         <input
           id="username-input"
           type="text"
           value={user.username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={(e) => updateUsername(e.target.value)}
         />
       </label>
       <label>
-        email <input id="email-input" type="text" />
+        email{' '}
+        <input
+          id="email-input"
+          type="text"
+          value={user.email}
+          onChange={(e) => updateEmail(e.target.value)}
+          onBlur={(e) => updateEmailBlur(e.target.value)}
+        />
       </label>
       <label>
-        password <input id="password-input" type="text" />
+        password{' '}
+        <input
+          id="password-input"
+          type="text"
+          value={user.password}
+          onChange={(e) => updatePassword(e.target.value)}
+        />
       </label>
       <label>
-        passwordConfirm <input id="passwordConfirm-input" type="text" />
+        passwordConfirm{' '}
+        <input
+          id="passwordConfirm-input"
+          type="text"
+          value={user.passwordConfirm}
+          onChange={(e) => updatePasswordConfirm(e.target.value)}
+        />
       </label>
 
-      <button type="submit">Odeslat</button>
+      <button type="submit" onClick={btnClick}>
+        Odeslat
+      </button>
     </form>
   );
 };
